79b7a9ef6757a61fe057056bb1e3f06ac8f9282d
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2022-03-01 12:38:52 +0100

    poniendo nav en login

--- login.php
+++ login.php
@@ -1,50 +1,58 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8"/>
-    <link rel="icon" href="img/logo/Favicon.png" type="image/png">
-    <link rel="stylesheet" href="css/estilos.css" type="text/css">
-	<title>Iniciar sesión</title>
-</head>
-<body>
-    <div class = "formulario">
-        <form action="" method="post">
-        <fieldset>
-            <legend>Datos personales</legend>
-                Nombre:<br> <input type="text" name="nom" required><br>
-                Contraseña:<br> <input type="password" name="pas" required><br>		
-        </fieldset>
-        <input type="submit" name="aceptar">	
-        </form>
-        <h3>Sino tines cuenta te puedes <a href = "registro.php" >registrar</a></h3>
-    </div>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="icon" href="img/logo/Favicon.png" type="image/png">
+        <link rel="stylesheet" href="css/estilos.css" type="text/css">
+        <title>Log In</title>
+    </head>
 
-    <?php
 
-        if(isset($_POST['nom']) && isset($_POST['pas'])){
-            $db = new mysqli('localhost', 'root', '', 'chesnutgames');
+    <body>
 
-            if($db->connect_error){
-                echo '<h1>No se ha podido conectar a la base de datos</h1>';
-            }
-            else{
-                $usuario = $_POST['nom'];
-                $contrsenia = $_POST['pas'];
+        <header>  
+                <?php 
+                    require("menu.php");       
+                ?>
+        </header>
+        <div class = "formulario">
+            <form action="" method="post">
+            <fieldset>
+                <legend>Datos personales</legend>
+                    Nombre:<br> <input type="text" name="nom" required><br>
+                    Contraseña:<br> <input type="password" name="pas" required><br>		
+            </fieldset>
+            <input type="submit" name="aceptar">	
+            </form>
+            <h3>Sino tines cuenta te puedes <a href = "registro.php" >registrar</a></h3>
+        </div>
 
-                $sql = "SELECT * FROM usuarios WHERE nombre = '$usuario' AND contrasenia = '$contrsenia'";
-                $q = $db->query($sql);
+        <?php
 
-                if($q->num_rows > 0){
-                    header("Location:index.php");
+            if(isset($_POST['nom']) && isset($_POST['pas'])){
+                $db = new mysqli('localhost', 'root', '', 'chesnutgames');
+
+                if($db->connect_error){
+                    echo '<h1>No se ha podido conectar a la base de datos</h1>';
                 }
                 else{
-                    echo '<h1>Usuario o contraseña no válido, sino te puedes <a href = "registro.php" >registrar</a></h1>';
+                    $usuario = $_POST['nom'];
+                    $contrsenia = $_POST['pas'];
+
+                    $sql = "SELECT * FROM usuarios WHERE nombre = '$usuario' AND contrasenia = '$contrsenia'";
+                    $q = $db->query($sql);
+
+                    if($q->num_rows > 0){
+                        header("Location:index.php");
+                    }
+                    else{
+                        echo '<h1>Usuario o contraseña no válido, sino te puedes <a href = "registro.php" >registrar</a></h1>';
+                    }
+
+                    $db->close();
                 }
-
-                $db->close();
             }
-        }
-    ?>
+        ?>
 
-</body>
+    </body>
 </html>
