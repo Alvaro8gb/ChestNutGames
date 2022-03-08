9ad01af1d559cf720cf391b8dac82121aaac336c
bausela <67244126+bausela@users.noreply.github.com>
2022-03-08 20:29:17 +0100

    Tablas de ranking y juegos(junto con inserts) añadidas

--- mysql/02-tablas.sql
+++ mysql/02-tablas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-04-2021 a las 17:17:01
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 7.4.16
+-- Tiempo de generación: 08-03-2022 a las 20:24:53
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,43 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `ejercicio3`
+-- Base de datos: `chesnutgames`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `Usuarios`
+-- Estructura de tabla para la tabla `juegos`
 --
 
-CREATE TABLE `Usuarios` (
+CREATE TABLE `juegos` (
+  `IdJuego` int(11) NOT NULL,
+  `Nombre` varchar(100) DEFAULT NULL,
+  `Imagen` longblob DEFAULT NULL,
+  `Descripcion` varchar(100) DEFAULT NULL,
+  `Categoria` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ranking`
+--
+
+CREATE TABLE `ranking` (
+  `IdJuego` int(11) DEFAULT NULL,
+  `IdJugador` int(11) DEFAULT NULL,
+  `Puntuacion` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombreUsuario` varchar(15) NOT NULL,
   `nombre` varchar(40) NOT NULL,
@@ -41,9 +68,25 @@ CREATE TABLE `Usuarios` (
 --
 
 --
--- Indices de la tabla `Usuarios`
+-- Indices de la tabla `juegos`
 --
-ALTER TABLE `Usuarios`
+ALTER TABLE `juegos`
+  ADD PRIMARY KEY (`IdJuego`),
+  ADD UNIQUE KEY `Nombre` (`Nombre`);
+
+--
+-- Indices de la tabla `ranking`
+--
+ALTER TABLE `ranking`
+  ADD KEY `IdJuego` (`IdJuego`),
+  ADD KEY `IdJugador` (`IdJugador`),
+  ADD KEY `IdJuego_2` (`IdJuego`),
+  ADD KEY `IdJugador_2` (`IdJugador`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -51,12 +94,30 @@ ALTER TABLE `Usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `Usuarios`
+-- AUTO_INCREMENT de la tabla `juegos`
 --
-ALTER TABLE `Usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `juegos`
+  MODIFY `IdJuego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ranking`
+--
+ALTER TABLE `ranking`
+  ADD CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`IdJugador`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ranking_ibfk_2` FOREIGN KEY (`IdJuego`) REFERENCES `juegos` (`IdJuego`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
