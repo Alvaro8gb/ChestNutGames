5dce005c63e0e943dd8b0e99b8895c3b8d7b7b55
PabloS-RS <79455013+PabloS-RS@users.noreply.github.com>
2022-03-09 09:24:07 +0100

    Update juegos.php

--- juegos.php
+++ juegos.php
@@ -4,15 +4,13 @@ require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Juegos';
 
-$contenidoPrincipal = "";
-
-    <<<EOS
+$contenidoPrincipal = <<<EOS
     <head>
         <link rel="stylesheet" type="text/css" href="css/estilo_juegos.css" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Juegos</title>;
     </head>
-    EOS;
+EOS;
 
     //<body>
     echo '<img class="gif_centrado" src="img/juegos/play_now.gif" alt="Gif">';
@@ -31,6 +29,5 @@ $contenidoPrincipal = "";
 
         
     </body>*/
-EOS;
 
 require __DIR__.'/includes/plantillas/plantilla.php';
