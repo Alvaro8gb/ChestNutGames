cb8dc79e4a7154c429a8207e9778bdccf0d07a3d
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2022-03-13 16:00:06 +0100

    arreglando codigo de Víctor

--- ranking.php
+++ ranking.php
@@ -4,98 +4,95 @@ require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Ranking';
 
-$jueg = 1;
+$juegos = array();
+
+$conn = $app->conexionBd();
+$sql = "SELECT IdJuego, nombre FROM juegos";
+$consulta = @mysqli_query($conn, $sql);
+
+
+while($fila = @mysqli_fetch_array($consulta)){
+  $juegos[$fila["IdJuego"]] = $fila["nombre"];
+}
 
 $contenidoPrincipal = <<<EOS
 <head>
-		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
         <link rel="stylesheet" type="text/css" href="css/slider.css" />
-		<link rel="icon" href="img/logo/Favicon.png" type="image/png">
-	</head>
 <body>
 <h2> Juegos </h2>
 <div class="container">
   <ul class="slider">
 EOS;
-    $conn = $app->conexionBd();
-    $sql = "SELECT IdJuego, Imagen FROM juegos";
-    $consulta = @mysqli_query($conn, $sql);
-    $parar = 0;
-while($parar != 1){
-  if($fila = @mysqli_fetch_array($consulta)){
+
+
+
+    foreach($juegos as $id_juego => $nombre){
+
+      $sql = sprintf("SELECT Imagen FROM juegos WHERE IdJuego = '%s'", $conn->real_escape_string($id_juego));
+      $consulta = @mysqli_query($conn, $sql);
+      $fila = @mysqli_fetch_array($consulta);
+
+      if ( $consulta->num_rows == 0 ) {
+        exit("tabla juegos sin imagenes");
+      }
+
       $contenidoPrincipal .= <<<EOS
-          <li id= {$fila["IdJuego"]}>
+      <li id= {$id_juego}>
       EOS;
-      $contenidoPrincipal .= '<img src="data:image/png;base64,'.base64_encode($fila["Imagen"]).'"/>';
+      $contenidoPrincipal .= '<a href="procesarJuego.php?id='.$id_juego.'"><img src="data:image/png;base64,'.base64_encode($fila["Imagen"]).'"/>';
       $contenidoPrincipal .= <<<EOS
-          </li>
+      </li>
       EOS;
-  }
-  else{
-      
-      $parar = 1;
-  }
-} 
-$parar = 0;
+
+    }
 
 $contenidoPrincipal .= <<<EOS
    </ul>
    <ul class="menu">
 EOS;
-  $sql = "SELECT IdJuego,Nombre FROM juegos";
-  $consulta = @mysqli_query($conn, $sql);
-  $concat;
-while($parar != 1){
-  if($fila = @mysqli_fetch_array($consulta)){
-    $concat = "#";
-    $concat.= $fila["IdJuego"];
+
+  foreach($juegos as $id_juego => $nombre){
+      $concat = "#";
+      $concat.= $id_juego;
+
       $contenidoPrincipal .= <<<EOS
-        <li>
-        <a href= {$concat} onclick= "tabla({$fila["IdJuego"]})"> {$fila["Nombre"]}</a>
-        </li>
-      EOS;
-  }
-  else{
-      
-      $parar = 1;
-  }
-} 
+          <li>
+          <a href= {$concat} onclick= "tabla({$id_juego})"> $nombre</a>
+          </li>
+        EOS;
+    }
+  
 $contenidoPrincipal .= <<<EOS
    </ul>
    </div>
    <h2> Jugadores</h2>
 EOS;
-$sql = "SELECT IdJugador, Puntuacion FROM ranking WHERE IdJuego = $jueg";
-$consulta = @mysqli_query($conn, $sql);
-while($consulta){
-  $parar = 0;
 
-  $contenidoPrincipal .= <<<EOS
-      <table id= {$jueg}>
+/*
+$sql = sprintf("SELECT IdJugador, Puntuacion FROM ranking WHERE IdJuego = '%s'", $conn->real_escape_string($jueg));
+$consulta = @mysqli_query($conn, $sql);
+
+$contenidoPrincipal .= <<<EOS
+        <table id= {$jueg}> 
+    EOS;
+    while($fila = @mysqli_fetch_array($consulta)){
+            $contenidoPrincipal .= <<<EOS
+                <tr>
+                <td>{$fila["IdJugador"]}</td>
+                <td>{$fila["Puntuacion"]}</td>
+                </tr>
+            EOS;
+            
+    }
+    
+    $contenidoPrincipal .= <<<EOS
+    </table>
   EOS;
-  while($parar != 1){
-        if($fila = @mysqli_fetch_array($consulta)){
-          $contenidoPrincipal .= <<<EOS
-              <tr>
-              <td>{$fila["IdJugador"]}</td>
-              <td>{$fila["Puntuacion"]}</td>
-              </tr>
-          EOS;
-          
-        }
-        else{
-          
-          $parar = 1;
-        }
-  }
-  $contenidoPrincipal .= <<<EOS
-  </table>
-EOS;
-  $sql = "SELECT IdJugador, Puntuacion FROM ranking WHERE IdJuego = $jueg";
-  $consulta = @mysqli_query($conn, $sql);
-  $jueg++;
-}
+
 $contenidoPrincipal .= <<<EOS
       </body>
   EOS;
+
+  */
+
 require __DIR__.'/includes/plantillas/plantilla.php';
