81ce59eb9783ef122377c968be599a42c1dcdb37
bausela <67244126+bausela@users.noreply.github.com>
2022-04-26 00:04:33 +0200

    Puta mierda de la tienda odio las bases de datos

--- mysql/02-tablas.sql
+++ mysql/02-tablas.sql
@@ -1,62 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-04-2022 a las 11:32:25
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `chestnutgames`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoriasjuegos`
---
 
 CREATE TABLE `categoriasjuegos` (
   `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categoriastienda`
---
-
 CREATE TABLE `categoriastienda` (
   `nombre` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+CREATE TABLE `tienda` (
+  `IdProducto` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `categoria` varchar(255) NOT NULL,
+  `descripcion` varchar(254) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `imagen` longblob NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Estructura de tabla para la tabla `compras`
---
 
 CREATE TABLE `compras` (
   `IdUsuario` int(11) NOT NULL,
-  `IdProducto` int(11) NOT NULL
+  `IdProducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `eventos`
---
 
 CREATE TABLE `eventos` (
   `IdEvento` int(11) NOT NULL,
@@ -69,22 +38,11 @@ CREATE TABLE `eventos` (
   `IdJuego` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `inscripcioneseventos`
---
 
 CREATE TABLE `inscripcioneseventos` (
   `IdUsuario` int(11) NOT NULL,
   `IdEvento` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `juegos`
---
 
 CREATE TABLE `juegos` (
   `IdJuego` int(11) NOT NULL,
@@ -95,12 +53,6 @@ CREATE TABLE `juegos` (
   `Enlace` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `publicidad`
---
-
 CREATE TABLE `publicidad` (
   `IdPublicidad` int(11) NOT NULL,
   `nombreEmpresa` varchar(255) NOT NULL,
@@ -109,37 +61,13 @@ CREATE TABLE `publicidad` (
   `imagen` longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ranking`
---
-
 CREATE TABLE `ranking` (
   `IdJuego` int(11) DEFAULT NULL,
   `IdJugador` int(11) DEFAULT NULL,
   `Puntuacion` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `tienda`
---
-
-CREATE TABLE `tienda` (
-  `IdProducto` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `categoria` varchar(255) NOT NULL,
-  `descripcion` varchar(254) NOT NULL,
-  `precio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `IdUsuario` int(11) NOT NULL,
@@ -150,154 +78,77 @@ CREATE TABLE `usuarios` (
   `rol` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categoriasjuegos`
---
 ALTER TABLE `categoriasjuegos`
   ADD PRIMARY KEY (`nombre`);
 
---
--- Indices de la tabla `categoriastienda`
---
 ALTER TABLE `categoriastienda`
   ADD PRIMARY KEY (`nombre`);
 
---
--- Indices de la tabla `compras`
---
-ALTER TABLE `compras`
-  ADD KEY `IdUsuario` (`IdUsuario`),
-  ADD KEY `IdProducto` (`IdProducto`);
-
---
--- Indices de la tabla `eventos`
---
-ALTER TABLE `eventos`
-  ADD PRIMARY KEY (`IdEvento`),
-  ADD KEY `IdJuego` (`IdJuego`);
-
---
--- Indices de la tabla `inscripcioneseventos`
---
-ALTER TABLE `inscripcioneseventos`
-  ADD KEY `IdUsuario` (`IdUsuario`),
-  ADD KEY `IdEvento` (`IdEvento`);
-
---
--- Indices de la tabla `juegos`
---
-ALTER TABLE `juegos`
-  ADD PRIMARY KEY (`IdJuego`),
-  ADD KEY `Categoria` (`Categoria`);
-
---
--- Indices de la tabla `publicidad`
---
-ALTER TABLE `publicidad`
-  ADD PRIMARY KEY (`IdPublicidad`);
-
---
--- Indices de la tabla `ranking`
---
-ALTER TABLE `ranking`
-  ADD KEY `IdJuego` (`IdJuego`),
-  ADD KEY `IdJugador` (`IdJugador`);
-
---
--- Indices de la tabla `tienda`
---
 ALTER TABLE `tienda`
   ADD PRIMARY KEY (`IdProducto`),
   ADD KEY `categoria` (`categoria`);
 
---
--- Indices de la tabla `usuarios`
---
+ALTER TABLE `compras`
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdProducto` (`IdProducto`);
+
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`IdEvento`),
+  ADD KEY `IdJuego` (`IdJuego`);
+
+ALTER TABLE `inscripcioneseventos`
+  ADD KEY `IdUsuario` (`IdUsuario`),
+  ADD KEY `IdEvento` (`IdEvento`);
+
+ALTER TABLE `juegos`
+  ADD PRIMARY KEY (`IdJuego`),
+  ADD KEY `Categoria` (`Categoria`);
+
+ALTER TABLE `publicidad`
+  ADD PRIMARY KEY (`IdPublicidad`);
+
+ALTER TABLE `ranking`
+  ADD KEY `IdJuego` (`IdJuego`),
+  ADD KEY `IdJugador` (`IdJugador`);
+
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`IdUsuario`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `eventos`
---
 ALTER TABLE `eventos`
   MODIFY `IdEvento` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `juegos`
---
 ALTER TABLE `juegos`
   MODIFY `IdJuego` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `publicidad`
---
 ALTER TABLE `publicidad`
   MODIFY `IdPublicidad` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `tienda`
---
-ALTER TABLE `tienda`
-  MODIFY `IdProducto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
 ALTER TABLE `usuarios`
   MODIFY `IdUsuario` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Restricciones para tablas volcadas
---
+ALTER TABLE `tienda`
+  MODIFY `IdProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
---
--- Filtros para la tabla `compras`
---
+
+
 ALTER TABLE `compras`
   ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`IdProducto`) REFERENCES `tienda` (`IdProducto`),
   ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`);
 
---
--- Filtros para la tabla `eventos`
---
 ALTER TABLE `eventos`
   ADD CONSTRAINT `eventos_ibfk_1` FOREIGN KEY (`IdJuego`) REFERENCES `juegos` (`IdJuego`);
 
---
--- Filtros para la tabla `inscripcioneseventos`
---
 ALTER TABLE `inscripcioneseventos`
   ADD CONSTRAINT `inscripcioneseventos_ibfk_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`),
   ADD CONSTRAINT `inscripcioneseventos_ibfk_2` FOREIGN KEY (`IdEvento`) REFERENCES `eventos` (`IdEvento`);
 
---
--- Filtros para la tabla `juegos`
---
 ALTER TABLE `juegos`
   ADD CONSTRAINT `juegos_ibfk_1` FOREIGN KEY (`Categoria`) REFERENCES `categoriasjuegos` (`nombre`);
 
---
--- Filtros para la tabla `ranking`
---
 ALTER TABLE `ranking`
   ADD CONSTRAINT `ranking_ibfk_1` FOREIGN KEY (`IdJuego`) REFERENCES `juegos` (`IdJuego`),
   ADD CONSTRAINT `ranking_ibfk_2` FOREIGN KEY (`IdJugador`) REFERENCES `usuarios` (`IdUsuario`);
 
---
--- Filtros para la tabla `tienda`
---
 ALTER TABLE `tienda`
   ADD CONSTRAINT `tienda_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoriastienda` (`nombre`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
