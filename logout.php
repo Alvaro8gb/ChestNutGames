c4555bc38253962c5f48a9305b683b3c8174ec08
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2022-04-27 12:53:10 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -7,6 +7,4 @@ $tituloPagina='Logout';
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantilla.php', $params);
 
-header("refresh:2 ,url=".$app->resuelve("/index.php"));
-exit();
 
