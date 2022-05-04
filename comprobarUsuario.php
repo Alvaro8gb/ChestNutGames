1b423c24ec6752699e67203b0ecd4d8fcdd37e4e
petio01bg <81573779+petio01bg@users.noreply.github.com>
2022-05-04 09:21:23 +0200

    Checkeo nombreUsuario

--- comprobarUsuario.php
+++ comprobarUsuario.php
@@ -1,6 +1,15 @@
 <?php
 
-	if ($_REQUEST["user"] === "admin") {
+use es\chestnut\usuarios\Usuario;
+
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/vistas/helpers/utils.php';
+
+	$user = $_REQUEST["user"];
+
+	$usuario = Usuario::buscarUsuarioPorNombre($user);
+
+	if ($usuario) {
 		echo "existe";
 	} else {
 		echo "disponible";
